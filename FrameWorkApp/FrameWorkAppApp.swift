bf4f53d518509acb3dc606a9b13ddaa127f94a3a
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2023-10-01 22:00:09 +0300

    Initial Commit

---
+++ FrameWorkApp/FrameWorkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameWorkAppApp.swift
+//  FrameWorkApp
+//
+//  Created by Edgars Jaudzems on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FrameWorkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
