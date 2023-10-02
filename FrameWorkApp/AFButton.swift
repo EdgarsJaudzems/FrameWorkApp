c2dd00ffcdfbcb7c62bcbf823d6bfa2be56c5644
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2023-10-02 10:41:05 +0300

    Update Detail view

---
+++ FrameWorkApp/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  FrameWorkApp
+//
+//  Created by edgars.jaudzems on 02/10/2023.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
