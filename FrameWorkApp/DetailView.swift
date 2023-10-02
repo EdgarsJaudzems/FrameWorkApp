0cd7fc2e4282df637d0ada1f6544ecdf461e6685
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2023-10-02 10:41:18 +0300

    Update Detail view

--- FrameWorkApp/DetailView.swift
+++ FrameWorkApp/DetailView.swift
@@ -8,24 +8,37 @@
 import SwiftUI
 
 struct DetailView: View {
+    
     var framework: Framework
     
     var body: some View {
         VStack {
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width:44, height: 44)
+                }
+            }.padding()
+            Spacer()
             FrameworkTitleView(framework: framework)
-            
-
-            Text("ARKit 4 introduces a brand-new Depth API, creating a new way to access the detailed depth information gathered by the LiDAR Scanner on iPhone 12 Pro, iPhone 12 Pro Max, and iPad Pro.\n\nLocation Anchors leverages the higher-resolution data in Apple Maps to place AR experiences at a specific point in the world in your iPhone and iPad apps. And support for face tracking extends to all devices with the Apple Neural Engine and a front-facing camera, so even more users can experience the joy of AR in photos and videos.")
-    Spacer()
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
             Button {
                 
             } label: {
-                Text("Learn More")
+                AFButton(title: "Learn More")
             }
         }
     }
 }
 
-//#Preview {
-//    DetailView()
-//}
+#Preview {
+    DetailView(framework: MockData.sampleFramework)
+}
